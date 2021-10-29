4b31ead0077e114a61ee60e54b43bd14e279b85d
yaojia <yaojia@blackjack-inc.com>
2021-10-29 11:48:16 +0800

    no message

---
+++ VfxTemp/Editor/PrefabExtendTools.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+static public class PrefabExtendTools
+{
+
+    [MenuItem("CONTEXT/Transform/OverridePrefab")]
+    static public void SavePrefab()
+    {
+        GameObject tarPrefab = PrefabUtility.GetCorrespondingObjectFromSource (Selection.activeGameObject) as GameObject;
+        //if(tarPrefab == null) return;
+        Debug.Log(tarPrefab);
+        string prefabPath = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(tarPrefab);
+        Debug.Log(prefabPath);
+        if(prefabPath.EndsWith(".prefab") == false) return;
+        AssetDatabase.LoadMainAssetAtPath(prefabPath);
+        Debug.Log(AssetDatabase.LoadMainAssetAtPath(prefabPath));
+        //PrefabUtility.ReplacePrefab (Selection.activeGameObject, , ReplacePrefabOptions.ConnectToPrefab | ReplacePrefabOptions.ReplaceNameBased);
+        //PrefabUtility.UnpackPrefabInstance(Selection.activeGameObject, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
+        PrefabUtility.SaveAsPrefabAssetAndConnect(Selection.activeGameObject, prefabPath, InteractionMode.AutomatedAction);
+    }
+}
---
+++ VfxTemp/Scripts/GetInspectorRotationValue.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace BlackJack.ProjectF.Client
+{
+	/// <summary>
+	/// 获取Unity面板中的物体的旋转值
+	/// </summary>
+	public class GetInspectorRotationValue : MonoBehaviour
+	{
+		public GameObject m_ObjectGo;
+		public bool m_UsePosition;
+		public bool m_UseRotateX;
+		public bool m_UseRotateY;
+		public bool m_UseRotateZ;
+		private void Update()
+		{
+			//位移模式
+			Vector3 GoTransform = m_ObjectGo.transform.position;
+			Vector3 thisTransform = this.transform.position;
+			Vector3 finalTransform = m_UsePosition ? GoTransform : thisTransform;
+			this.GetComponent<Transform>().position = finalTransform;
+			
+			//旋转模式
+			//自我旋转
+			float thisRotatex = this.transform.localEulerAngles.x;
+			float thisRotatey = this.transform.localEulerAngles.y;
+			float thisRotatez = this.transform.localEulerAngles.z;		
+			//目标旋转
+			float GoRotatex = m_ObjectGo.transform.localEulerAngles.x;
+			float GoRotatey = m_ObjectGo.transform.localEulerAngles.y;
+			float GoRotatez = m_ObjectGo.transform.localEulerAngles.z;
+			
+			//开关取值
+			float finalRotatex = m_UseRotateX ? GoRotatex : thisRotatex;
+			float finalRotatey = m_UseRotateY ? GoRotatey : thisRotatey;
+			float finalRotatez = m_UseRotateZ ? GoRotatez : thisRotatez;
+			
+			this.GetComponent<Transform>().rotation = Quaternion.Euler(finalRotatex, finalRotatey, finalRotatez);
+		
+		}
+	}
+}
+
