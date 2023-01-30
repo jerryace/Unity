dbd9dbefe3bbe3bd8bf9ed60d3f4d0ca7c89e59c
JerryAce <38008500+jerryace@users.noreply.github.com>
2023-01-30 14:18:53 +0800

    Fx nested prefab save
    保存嵌套在pfb内的子pfb修改。

---
+++ VfxTemp/Editor/FxNestedPrefabSave.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace BlackJack.ProjectF.Editor
+{
+    /// <summary>
+    /// 嵌套预制体保存 Transform组件右键功能
+    /// </summary>
+    public class FxNestedPrefabSave
+    {
+        [MenuItem("GameObject/ProjectF/Save Nested Prefab")]
+        public static void SaveNestPrefab()
+        {
+            GameObject originGo = Selection.activeGameObject;
+            string path = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(originGo);
+
+            //通过PrefabUtility查找当前选中物件最近的prefabGameObjectRoot。Duplicate要对这个root操作
+            Selection.activeObject = PrefabUtility.GetNearestPrefabInstanceRoot(Selection.activeGameObject);
+            Unsupported.DuplicateGameObjectsUsingPasteboard();
+            
+            GameObject duplicate = Selection.activeGameObject;
+            PrefabUtility.SaveAsPrefabAssetAndConnect(duplicate, path, InteractionMode.AutomatedAction, out bool pfbSuccess);
+            Object.DestroyImmediate(duplicate);
+
+            Selection.activeGameObject = originGo;
+
+            if (pfbSuccess == false)
+            {
+                Debug.Log("保存失败");
+                return;
+            }
+            Debug.Log("保存完成");
+        }
+    }
+}
